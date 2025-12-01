bf68782055b7511e94f5cc55d3dbaa0391856d8d
rayk47 <raymondjaykay@gmail.com>
2025-11-30 18:08:12 -0800

    fix(api): incorrect replace string

--- packages/api/src/utils/generate-types.ts
+++ packages/api/src/utils/generate-types.ts
@@ -61,7 +61,10 @@ const buildJsonSchemaTypes = async (yamlFile: string) => {
             temp = temp + `export const ${key}Schema = ${JSON.stringify(value)};`;
         }
 
-        temp = temp.replace(/"format": "date"/g, '"pattern":"^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$"');
+        temp = temp.replace(
+            /"format":"date"/g,
+            '"pattern":"^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$"'
+        );
         temp = temp.replace(/"type":"object"/g, '"type":JsonSchemaType.OBJECT');
         temp = temp.replace(/"type":"string"/g, '"type":JsonSchemaType.STRING');
         temp = temp.replace(/"type":"array"/g, '"type":JsonSchemaType.ARRAY');
